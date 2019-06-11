77fd3dd93109bc00e9239aef17fdc525dd6579f1
chetansharmauit <chetansharmauit@gmail.com>
2019-06-11 21:49:17 +0530

    first commit

---
+++ src/launch/launchchrome.java
@@ -0,0 +1,16 @@
+package launch;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class launchchrome {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver","./exefiles/chromedriver.exe");
+		ChromeDriver c = new ChromeDriver();
+		
+		
+	}
+
+}
